ecc69e0154a5840bddb798c9bf73c5575ad46bc3
Cassetu <cassetu@example.com>
2025-06-16 09:58:37 -0700

    Start

---
+++ src/main/java/cassetu/arcaneimbued/ArcaneImbued.java
@@ -0,0 +1,17 @@
+package cassetu.arcaneimbued;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArcaneImbued implements ModInitializer {
+	public static final String MOD_ID = "arcane-imbued";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/cassetu/arcaneimbued/ArcaneImbuedClient.java
@@ -0,0 +1,10 @@
+package cassetu.arcaneimbued;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneImbuedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
